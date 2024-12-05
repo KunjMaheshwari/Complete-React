bb3fa43bb03ef84d83fd8c1ad341153222d73910
Kunj Maheshwari <kunj.maheshwari2021@vitbhopal.ac.in>
2024-12-05 18:52:31 +0530

    Added till Props Concept.

---
+++ ReactEssential1/01-starting-project/src/components/Main.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import CoreConcept from './CoreConcept'
+import componentsImg from "/src/assets/components.png";
+import { CORE_CONCEPTS } from '../data.js';
+
+const Main = () => {
+  return (
+    <div>
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} 
+            /> 
+            <CoreConcept
+              title={CORE_CONCEPTS[2].title}
+              description={CORE_CONCEPTS[2].description}
+              image={CORE_CONCEPTS[2].image}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+              image={CORE_CONCEPTS[3].image}
+            />
+          </ul>
+        </section>
+      </main>
+    </div>
+  )
+}
+
+export default Main
